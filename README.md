75bb368289c344117756766118b1d16357092f6e
xie hao <sei_xh2006@126.com>
2015-12-15 00:23:52 +0800

    so_reuseaddr

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # server
+
+使用原生java socket开发，主要用于了解socket特性以及实验，真实场景中依旧推荐使用netty。
--- src/main/java/io/xhao/server/simple/SimpleServer.java
+++ src/main/java/io/xhao/server/simple/SimpleServer.java
@@ -3,6 +3,7 @@ package io.xhao.server.simple;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,7 +12,9 @@ public class SimpleServer {
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
 		try {
-			serverSocket = new ServerSocket(8080);
+			serverSocket = new ServerSocket();
+			serverSocket.setReuseAddress(true);
+			serverSocket.bind(new InetSocketAddress(8080));
 			while (true) {
 				Socket client = serverSocket.accept();
 				new Thread(new Handler(client)).start();
